902d8dcfa76a16c9122fb755f6073992337e684f
doubleface <christophe@thiriot.xyz>
2017-07-04 11:34:11 +0200

    feat: first implementation saving toots from the home

---
+++ index.js
@@ -0,0 +1,30 @@
+const { BaseKonnector, saveData } = require('cozy-konnector-libs')
+let request = require('request-promise')
+// require('request-debug')(request)
+
+const DOCTYPE = 'io.cozy.mastodonstatuses'
+
+module.exports = new BaseKonnector(fields => {
+  console.log(fields, 'fields')
+  // first get the access token
+  const {client_id, client_secret, username, password} = fields
+  return request({
+    method: 'POST',
+    json: true,
+    uri: `${fields.url}/oauth/token`,
+    form: { client_id, client_secret, username, password, grant_type: 'password' }
+  })
+  .then(body => {
+    return request({
+      headers: {
+        'Authorization': `Bearer ${body.access_token}`
+      },
+      json: true,
+      uri: `${fields.url}/api/v1/timelines/home`
+    })
+  })
+  .then(toots => {
+    return saveData(toots, DOCTYPE)
+  })
+  .catch(err => console.error(err))
+})
