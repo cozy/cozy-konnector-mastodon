580e4a8b6f609a346056cd33a5433b25d0d917ea
doubleface <christophe@thiriot.xyz>
2017-07-05 17:53:03 +0200

    Now can save last toot id in the account data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { BaseKonnector, filterData, saveData } = require('cozy-konnector-libs')
+const { BaseKonnector, log, filterData, addData } = require('cozy-konnector-libs')
 let request = require('request-promise')
 // require('request-debug')(request)
 
 const DOCTYPE = 'io.cozy.mastodonstatuses'
 
-module.exports = new BaseKonnector(fields => {
+module.exports = new BaseKonnector(function (fields) {
   // first get the access token
   const {client_id, client_secret, username, password} = fields
   return request({
@@ -14,15 +14,28 @@ module.exports = new BaseKonnector(fields => {
     form: { client_id, client_secret, username, password, grant_type: 'password' }
   })
   .then(body => {
+    const { lastTootId } = this.getAccountData()
+    log('debug', lastTootId, 'got this last toot id from account data')
+
     return request({
       headers: {
         'Authorization': `Bearer ${body.access_token}`
+      },
+      qs: {
+        since_id: lastTootId
       },
       json: true,
       uri: `${fields.url}/api/v1/timelines/home`
     })
   })
   .then(toots => filterData(toots, DOCTYPE, {keys: ['id']}))
-  .then(toots => saveData(toots, DOCTYPE))
+  .then(toots => addData(toots, DOCTYPE))
+  .then(toots => {
+    if (toots.length) {
+      const lastTootId = toots[0].id
+      log('debug', lastTootId, 'saving new last toot id')
+      return this.saveAccountData({lastTootId})
+    }
+  })
   .catch(err => console.error(err))
 })
