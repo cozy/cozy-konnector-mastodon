3b10dd09e495d674f8c0d534f761857b768e648c
doubleface <christophe@thiriot.xyz>
2017-07-05 11:27:14 +0200

    feat: added use of filterData

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const { BaseKonnector, saveData } = require('cozy-konnector-libs')
+const { BaseKonnector, filterData, saveData } = require('cozy-konnector-libs')
 let request = require('request-promise')
 // require('request-debug')(request)
 
 const DOCTYPE = 'io.cozy.mastodonstatuses'
 
 module.exports = new BaseKonnector(fields => {
-  console.log(fields, 'fields')
   // first get the access token
   const {client_id, client_secret, username, password} = fields
   return request({
@@ -23,8 +22,7 @@ module.exports = new BaseKonnector(fields => {
       uri: `${fields.url}/api/v1/timelines/home`
     })
   })
-  .then(toots => {
-    return saveData(toots, DOCTYPE)
-  })
+  .then(toots => filterData(toots, DOCTYPE, {keys: ['id']}))
+  .then(toots => saveData(toots, DOCTYPE))
   .catch(err => console.error(err))
 })
